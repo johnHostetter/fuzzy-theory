be1b05b38e3af02e25db35c39c9b5fc007e8f06c
John Wesley Hostetter <jhostetter16@gmail.com>
2024-08-23 18:56:52 -0400

    Add unit test skeleton for linkage

---
+++ tests/test_relations/test_linkage.py
@@ -0,0 +1,17 @@
+import unittest
+
+
+class TestBinaryLinks(unittest.TestCase):
+    def test_create_linkage(self):
+        raise NotImplementedError
+
+    def test_save_and_load_linkage(self):
+        raise NotImplementedError
+
+
+class TestGroupedLinks(unittest.TestCase):
+    def test_create_linkage(self):
+        raise NotImplementedError
+
+    def test_save_and_load_linkage(self):
+        raise NotImplementedError
