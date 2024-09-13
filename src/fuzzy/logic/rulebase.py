Fix calculation of RuleBase shape

src/fuzzy/logic/rulebase.py
@@ -68,7 +68,7 @@ class RuleBase(torch.nn.Module):
         return Shape(
             n_inputs=self.premises.shape[0],
             n_input_terms=self.premises.shape[1],
-            n_rules=self.premises.shape[2],
+            n_rules=len(self.rules),
             n_outputs=self.consequences.shape[0],
             n_output_terms=self.consequences.shape[1],
         )