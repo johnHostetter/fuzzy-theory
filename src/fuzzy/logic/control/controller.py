94e1070abebd13f298d45c396cde6dd9782bb096
John Wesley Hostetter <jhostetter16@gmail.com>
2025-10-30 17:50:07 -0500

    Remove unnecessary save/load code from FLC

--- src/fuzzy/logic/control/controller.py
+++ src/fuzzy/logic/control/controller.py
@@ -118,16 +118,6 @@ class FuzzyLogicController(torch.nn.Sequential):
             path / "input"
         )  # save the input granulation layer (drop the extension)
         self.engine.save(path / "engine.pt")  # save the inference engine
-        # TODO: figure out a better way to handle the configuration
-        if hasattr(self.engine, "configuration"):
-            # pickle the configuration for the engine
-            with open(path / "engine_config.pkl", "wb+") as f:
-                pickle.dump(
-                    self.engine.configuration, f, protocol=pickle.HIGHEST_PROTOCOL
-                )
-            # save the layer_norm weights
-            torch.save(self.engine.layer_norm.state_dict(), path / "layer_norm.pt")
-
         self.defuzzification.save(
             path / "defuzzification"
         )  # save the defuzzification method
@@ -148,21 +138,11 @@ class FuzzyLogicController(torch.nn.Sequential):
         # load the components from their respective directories
         input_granules = FuzzySetGroup.load(path / "input", device=device)
         engine = TNorm.load(path / "engine", device=device)
-        # TODO: figure out a better way to handle the configuration
-        if hasattr(engine, "configuration"):
-            # pickle the configuration for the engine
-            with open(path / "engine_config.pkl", "rb") as f:
-                engine.configuration = pickle.load(f)
-            # load the layer_norm weights
-            engine.layer_norm.load_state_dict(
-                torch.load(path / "layer_norm.pt", map_location=device)
-            )
-
         defuzzification = Defuzzification.load(path / "defuzzification", device=device)
 
         # load the FLC state dictionary for the remaining components
         state_dict: MutableMapping[str, Any] = torch.load(
-            path / "flc.pt", map_location=device
+            path / "flc.pt", map_location=device, weights_only=False
         )
         shape: Shape = Shape(*state_dict.pop("shape"))
 
